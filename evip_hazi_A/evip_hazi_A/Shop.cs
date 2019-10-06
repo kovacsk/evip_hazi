32daa7aad901213e225dfe7f6e4d732c619c1759
Kriszitán Kovács <kovacsk88@gmail.com>
2019-10-06 13:19:58 +0200

    GetPrice függvény

--- evip_hazi_A/evip_hazi_A/Shop.cs
+++ evip_hazi_A/evip_hazi_A/Shop.cs
@@ -8,9 +8,29 @@ namespace evip_hazi_A
 {
     class Shop
     {
+        IDictionary<string, int> productlist = new Dictionary<string, int>();
+
 
         public void RegisterProduct(String nev, int ertek)
         {
+            productlist.Add(new KeyValuePair<string, int>(nev, ertek));
+
+        }
+
+        public void GetPrice(string n)
+        {
+            string[] characters = n.Split();
+            int price = 0;
+
+            for (int i = 0; i < characters.Length; i++) { 
+                foreach (KeyValuePair<string, int> item in productlist)
+                {
+                    if (characters[i] == item.Key)
+                    {
+                        price += item.Value;
+                    }
+                }
+        }
 
         }
     }
