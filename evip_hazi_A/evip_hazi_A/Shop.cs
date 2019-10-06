e37415c7272bd267cf5f4f5eab9df8a9ae082d79
Kriszitán Kovács <kovacsk88@gmail.com>
2019-10-06 11:15:48 +0200

    shop elkeszites

---
+++ evip_hazi_A/evip_hazi_A/Shop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evip_hazi_A
+{
+    class Shop
+    {
+
+        public void RegisterProduct(String nev, int ertek)
+        {
+
+        }
+    }
+}
